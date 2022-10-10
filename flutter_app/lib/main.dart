387500e861bd60fc4931cd5f9b4fc56a1fec3e2d
Jon Loh <jon.loh@hotmail.com>
2022-10-10 17:08:39 +0800

    creates the same app using stateful widgets to change account level

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -4,72 +4,70 @@ import 'package:flutter/material.dart';
 void main() => runApp(MaterialApp(
     // <root widget> acts as a wrapper for other widgets inside it
     home: Card() // determines what will show on the home page
-));
+    ));
 
-class Card extends StatelessWidget {
+class Card extends StatefulWidget {
   const Card({super.key});
+
+  @override
+  State<Card> createState() => _CardState();
+}
+
+class _CardState extends State<Card> {
+  int level = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[900],
-
       appBar: AppBar(
         title: Text('ID Card'),
         centerTitle: true,
         backgroundColor: Colors.grey[850],
         elevation: 0.0,
       ),
-
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() { 
+            // must use setState function to change/update value in app
+            // setState() triggers the build function to re-run again with the updated data
+            level += 1;
+          });
+        },
+        child: Icon(Icons.add),
+        backgroundColor: Colors.grey[800],
+      ),
       body: Padding(
         padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-
             Row(
               children: <Widget>[
                 CircleAvatar(
                   backgroundImage: AssetImage('assets/user.png'),
                   radius: 40.0,
                 ),
-
                 SizedBox(width: 30.0),
-
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
-                    Text(
-                      'NAME',
-                      style: TextStyle(
-                        color: Colors.grey,
-                        letterSpacing: 1.5
-                      )
-                    ),
-
+                    Text('NAME',
+                        style:
+                            TextStyle(color: Colors.grey, letterSpacing: 1.5)),
                     SizedBox(height: 10.0),
-
-                    Text(
-                      'JON LOH',
-                      style: TextStyle(
-                        color: Colors.amberAccent[200],
-                        fontWeight: FontWeight.bold,
-                        fontSize: 28.0,
-                        letterSpacing: 1.5
-                      )
-                    )
+                    Text('JON LOH',
+                        style: TextStyle(
+                            color: Colors.amberAccent[200],
+                            fontWeight: FontWeight.bold,
+                            fontSize: 28.0,
+                            letterSpacing: 1.5))
                   ],
                 )
               ],
             ),
-
-            Divider(
-              height: 60.0,
-              color: Colors.grey[800]
-            ),
-
+            Divider(height: 60.0, color: Colors.grey[800]),
             SizedBox(height: 30.0),
-
             Text(
               'CURRENT ACCOUNT LEVEL',
               style: TextStyle(
@@ -77,11 +75,9 @@ class Card extends StatelessWidget {
                 letterSpacing: 1.5,
               ),
             ),
-
             SizedBox(height: 10.0),
-            
             Text(
-              '8',
+              '$level',
               style: TextStyle(
                 color: Colors.amberAccent[200],
                 letterSpacing: 1.5,
@@ -89,18 +85,14 @@ class Card extends StatelessWidget {
                 fontWeight: FontWeight.bold,
               ),
             ),
-
             SizedBox(height: 30.0),
-
             Row(
               children: [
                 Icon(
                   Icons.email,
                   color: Colors.grey[400],
                 ),
-
                 SizedBox(width: 10.0),
-
                 Text(
                   'jonloh@hotmail.com',
                   style: TextStyle(
@@ -111,29 +103,22 @@ class Card extends StatelessWidget {
                 ),
               ],
             ),
-            
             SizedBox(height: 10.0),
-
             Row(
               children: [
                 Icon(
                   Icons.phone,
                   color: Colors.grey[400],
                 ),
-
                 SizedBox(width: 10.0),
-
-                Text(
-                  '12345678',
-                  style: TextStyle(
-                    color: Colors.grey[400],
-                    fontSize: 18.0,
-                    letterSpacing: 1.0,
-                  )
-                )
+                Text('12345678',
+                    style: TextStyle(
+                      color: Colors.grey[400],
+                      fontSize: 18.0,
+                      letterSpacing: 1.0,
+                    ))
               ],
             )
-
           ],
         ),
       ),
